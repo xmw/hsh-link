5ab84861eeba8739a1d843ba6fee9c5cf467983a
Michael Weber <michael@xmw.de>
2014-09-02 22:16:56 +0200

    working version

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 STORAGE_DIR = '/srv/http/de.xmw.clip/data'
+LINK_DIR = '/srv/http/de.xmw.clip/link'
--- xmw_clip.py
+++ xmw_clip.py
@@ -1,19 +1,11 @@
 # vim: tabstop=4
 # copyright Michael Weber (michael at xmw dot de) 2014
 
+from config import STORAGE_DIR, LINK_DIR
+from mod_python.apache import OK, HTTP_NOT_FOUND
 
-#from re import match
-#from urllib import unquote
-from mod_python import apache, util
-#from mod_python.util import redirect
+import hashlib, mod_python, os
 
-from config import STORAGE_DIR
-
-import hashlib, os
-
-
-get_query = lambda req, key: dict(map(lambda s: s.split('=', 1), req.args.split('&'))).get(key, None)
-remote_inet = lambda req: req.get_remote_host(apache.REMOTE_NOLOOKUP, True)[0]
 
 def encapsulate(req, raw, html=False):
     if html:
@@ -21,7 +13,9 @@ def encapsulate(req, raw, html=False):
         req.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" ' + \
             '"http://www.w3.org/TR/html4/loose.dtd">\n')
         req.write('<HTML>\n<HEAD>\n<META http-equiv="Content-Type" content="text/html; charset=utf-8">\n')
-        req.write('<TITLE>%s</TITLE>\n</HEAD>\n<BODY>\n<CENTER>' % req.headers_in['Host'])
+        req.write('<script>function hide() { document.getElementById("submit").style.visibility="hidden"; }')
+        req.write('  function show() { document.getElementById("submit").style.visibility="inherit"; }</script>')
+        req.write('<TITLE>%s</TITLE>\n</HEAD>\n<BODY onLoad="hide()">\n<CENTER>' % req.headers_in['Host'])
     else:
         req.content_type = "text/plain; charset=utf-8"
 
@@ -47,56 +41,82 @@ def write_storage(fn, data):
     
        
 def handler(req):
-    
-    create = False
     agent = req.headers_in.get('User-Agent', '').lower()
     html = agent.count('mozilla') or agent.count('opera') or agent.count('validator')
+    var = mod_python.util.FieldStorage(req, keep_blank_values=True)
+    if var.getlist('raw'):
+        html = False
 
-    #lookup
-    obj = os.path.normpath(req.uri)[1:]
-    if not obj:
-        encapsulate(req, "add clipboard name as URI path!\n", html)
-        return apache.OK
-
-    fn = os.path.join(STORAGE_DIR, obj)
-    if not os.path.exists(fn):
-        obj = hashlib.sha1(obj).hexdigest()
-        fn = os.path.join(STORAGE_DIR, obj)
-
-    # update action
-    new_content = None
-    if req.method == 'GET':
-        if req.args:
-            get = util.FieldStorage(req, keep_blank_values=True)
-            if get.getlist('raw'):
-                html = False
-            new_content = get.getfirst('content')
+    # new_content
+    if req.method in ('DELETE', 'PUT'):
+        encapsulate(req, "future DELETE and PUT not yet implented.\n", html)
+        return OK
+    elif req.method == 'GET':
+        new_content = var.getfirst('content')
     elif req.method == 'POST':
-        get = util.FieldStorage(req, keep_blank_values=True)
-        if get.getlist('raw'):
-            html = False
-        new_content = get.getfirst('content')
-    elif req.method == 'DELETE':
-        if os.path.exists(fn):
-            os.unlink(fn)
+        if req.args:
+            encapsulate(req, "Mixed POST and GET not supported.\n", html)
+            return OK
+        new_content = var.getfirst('content')
+
+    # lookup
+    obj = os.path.normpath(req.uri)[1:]
+    link = None
+    blob = None
+    if obj:
+        if os.path.exists(os.path.join(STORAGE_DIR, obj)):
+            blob = obj
+        else:
+            cand = filter(lambda s: s.startswith(obj), os.listdir(STORAGE_DIR))
+            if cand:
+                blob = sorted(cand, key=lambda fn: os.lstat(os.path.join(STORAGE_DIR, fn)).st_ctime)[0]
+        if not blob:
+            link = obj
+            if os.path.exists(os.path.join(LINK_DIR, link)):
+                blob = read_storage(os.path.join(LINK_DIR, link))
+
+    if blob and os.path.exists(os.path.join(STORAGE_DIR, blob)):
+        content = read_storage(os.path.join(STORAGE_DIR, blob))
     else:
-        encapsulate(req, "Invalid method!\n", html)
-        return apache.OK
-    
+        content = ""
+
+    # append
+    if not new_content:
+        append_content = var.getfirst('append')
+        if append_content:
+            new_content = content + append_content
+
+    # store
     if new_content:
-            write_storage(fn, new_content)
-
-    #read
-    content = read_storage(fn) or ""
-
+        new_blob = hashlib.sha1(new_content).hexdigest()
+        if new_blob != blob:
+            write_storage(os.path.join(STORAGE_DIR, new_blob), new_content)
+            if link:
+                write_storage(os.path.join(LINK_DIR, link), new_blob)
+            if html:
+                mod_python.util.redirect(req, "/%s" % (link or new_blob), new_blob)
+    
     #output
     if html:
-        encapsulate(req, """%s
-<FORM method="POST" action="%s"><BR>
-  <TEXTAREA name="content">%s</TEXTAREA><BR>
-  <INPUT type="submit" value="update"><INPUT type="reset" value="reset">
-</FORM>""" % (content and obj or "new clipboard", req.uri, content), True)
+        text = []
+        text.append('<FORM enctype="multipart/form-data" method="POST" action="%s">' % (link or '/'))
+        text.append('<TEXTAREA placeholder="Start typing ..." border="0" cols="81" rows="24" name="content" oninput="show()">%s</TEXTAREA><BR>' % (new_content or content))
+        if content:
+            if link:
+                text.append('<A href="%s" text="mutable tag: %s">symlink</A>' % (link, link))
+            if blob:
+                text.append('<A href="%s" text="immutable hash: %s">context</A>' % (blob, blob))
+                text.append('<INPUT type="hidden" name="prev" value="%s">' % blob)
+        text.append('<INPUT type="submit" id="submit" value="store">')
+        text.append('</FORM>')
+        if var.getlist('debug'):
+            text.append(str(req.headers_in))
+        encapsulate(req, '\n'.join(text), True)
     else:
-        encapsulate(req, content, html)
+        if new_content:
+            encapsulate(req, req.headers_in['Host'] + '/' + new_blob, False)
+        else:
+            encapsulate(req, content, False)
+            
 
-    return apache.OK
+    return OK
